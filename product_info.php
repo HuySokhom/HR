cf13b00c081e46085f9b3c221d19758a48ce5d90
John Ny <kom.huy@gmail.com>
2016-08-19 15:08:12 +0700

    update master

--- product_info.php
+++ product_info.php
@@ -12,33 +12,32 @@
 		select
 			p.products_id,
 			p.customers_id,
+			cu.company_name,
+			cu.photo_thumbnail,
+			cu.customers_email_address,
+			cu.customers_address,
+			cu.customers_website,
+			cu.customers_telephone,
 			p.products_kind_of,
-			p.products_image_thumbnail,
+			p.gender,
+			p.number_of_hire,
 			pd.products_name,
-			v.name_en as village_name,
 			l.name as province_name,
-			d.name_en as district_name,
 			pd.products_description,
-			p.map_lat,
-			p.map_long,
-			p.bed_rooms,
-			p.bath_rooms,
-			p.number_of_floors,
+			pd.skill,
+			pd.benefits,
 			pd.products_viewed,
-			p.products_image,
-			p.products_price,
-			p.products_tax_class_id,
-			p.products_date_added
+			p.salary,
+			p.products_date_added,
+			p.products_close_date
 		from
-			" . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, location l, village v, district d
+			" . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, location l, customers cu
 		where
 			p.products_status = '1'
 				and
-			v.id = p.village_id
+			cu.customers_id = p.customers_id
 				and
 			l.id = p.province_id
-				and
-			d.id = p.district_id
 				and
 			p.products_id = '" . (int)$HTTP_GET_VARS['products_id'] . "'
 				and
@@ -46,7 +45,7 @@
 				and
 			pd.language_id = '" . (int)$languages_id . "'
 	");
-	$product_info = tep_db_fetch_array($product_info_query);
+	$product_info = tep_db_fetch_array($product_info_query);var_dump($product_info);
 ?>
 <br>
 <div class="container">
@@ -84,104 +83,101 @@
 		  <div class="col-sm-8">
 			  <div class="position-header">
 				  <h1>
-					  Senior Java Developer
+					  <?php echo $product_info['products_name'];?>
 					  <span>Urgent</span>
 				  </h1>
-
-				  <h2>
-				<span class="position-header-company-image">
-					<a href="company-detail.html">
-						<img src="assets/img/tmp/dropbox.png" alt="">
-					</a>
-				</span>
-
-					  <a href="company-detail.html">
-						  Dropbox
-					  </a>
-				  </h2>
 			  </div><!-- /.position-header -->
 
 			  <div class="position-general-information">
 				  <dl>
 					  <dt>Location</dt>
-					  <dd>San Fracisco, California</dd>
+					  <dd><?php echo $product_info['province_name'];?></dd>
 
-					  <dt>Start Date</dt>
-					  <dd>28/11/2015</dd>
+					  <dt>Public Date</dt>
+					  <dd><?php echo $product_info['products_date_added'];?></dd>
+					  <dt>Close Date</dt>
+					  <dd><?php echo $product_info['products_close_date'];?></dd>
 
 					  <dt>Contract</dt>
-					  <dd>Full time</dd>
+					  <dd><?php echo $product_info['products_kind_of'];?></dd>
 
 					  <dt>Salary</dt>
-					  <dd>By agreement</dd>
+					  <dd>
+						  <?php
+						  	if($product_info['salary'] > 0){
+								echo '$'.$product_info['salary'];
+							}else{
+								echo "Negotiable";
+							}
+						  ?>
+					  </dd>
+
+					  <dt>Number Of Hire</dt>
+					  <dd><?php echo $product_info['number_of_hire'];?></dd>
+
+					  <dt>Gender</dt>
+					  <dd><?php echo $product_info['gender'];?></dd>
 
 					  <dt>Job ID</dt>
-					  <dd>#1234</dd>
+					  <dd>#<?php echo $product_info['products_id'];?></dd>
+
+					  <dt>View</dt>
+					  <dd>
+						  <?php echo $product_info['products_viewed'];?>
+					  </dd>
 				  </dl>
 			  </div><!-- /.position-general-information -->
 
 			  <h3 class="page-header">Description, duties, responsibilities</h3>
 			  <p>
-				  Vivamus dignissim ex eu diam eleifend pharetra. Aliquam eleifend arcu quis risus scelerisque feugiat. Donec suscipit tincidunt purus et vulputate. Proin ac rutrum urna, nec elementum leo. Praesent commodo neque nunc, efficitur aliquam quam iaculis a. Sed quis eros justo. Pellentesque ut turpis quam.
+				  <?php echo $product_info['products_description'];?>
 			  </p>
 
 			  <h3 class="page-header">Other benefits</h3>
-
-			  <ul>
-				  <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-				  <li>Aliquam tincidunt mauris eu risus.</li>
-				  <li>Vestibulum auctor dapibus neque.</li>
-				  <li>Nunc dignissim risus id metus.</li>
-				  <li>Cras ornare tristique elit.</li>
-				  <li>Vivamus vestibulum nulla nec ante.</li>
-				  <li>Praesent placerat risus quis eros.</li>
-				  <li>Fusce pellentesque suscipit nibh.</li>
-				  <li>Integer vitae libero ac risus egestas placerat.</li>
-				  <li>Vestibulum commodo felis quis tortor.</li>
-				  <li>Ut aliquam sollicitudin leo.</li>
-				  <li>Cras iaculis ultricies nulla.</li>
-				  <li>Donec quis dui at dolor tempor interdum.</li>
-				  <li>Vivamus molestie gravida turpis.</li>
-				  <li>Fusce lobortis lorem at ipsum semper sagittis.</li>
-				  <li>Nam convallis pellentesque nisl.</li>
-				  <li>Integer malesuada commodo nulla.</li>
-			  </ul>
+			  <?php echo $product_info['benefits'];?>
 
 			  <h3 class="page-header">Personality requirements and skills</h3>
+			  <?php echo $product_info['skill'];?>
 
-			  <ul>
-				  <li>Ut aliquam sollicitudin leo.</li>
-				  <li>Cras iaculis ultricies nulla.</li>
-				  <li>Donec quis dui at dolor tempor interdum.</li>
-				  <li>Vivamus molestie gravida turpis.</li>
-				  <li>Fusce lobortis lorem at ipsum semper sagittis.</li>
-			  </ul>
 		  </div><!-- /.col-* -->
 
 		  <div class="col-sm-4">
 			  <div class="company-card">
 				  <div class="company-card-image">
-					  <span>Top Employeer</span>
-					  <a href="company-detail.html">
-						  <img src="assets/img/tmp/dropbox.png" alt=""></a>
+					  <a href="<?php tep_href_link(FILENAME_INFORMATION, 'info_id=' . $product_info['customers_id']) ?>">
+						  <img src="<?php echo $product_info['photo_thumbnail'];?>" alt=""></a>
 					  </a>
 				  </div><!-- /.company-card-image -->
 
 				  <div class="company-card-data">
 					  <dl>
 						  <dt>Website</dt>
-						  <dd><a href="http://example.com">www.example.com</a></dd>
+						  <dd>
+							  <?php
+								  if($product_info['customers_website']){
+									  echo '
+										  <a href="http://' . $product_info['customers_website'] . '" target="_blank">
+											  ' . $product_info['customers_website'] . '
+										  </a>';
+								  }else{
+									  echo 'N/A';
+								  }
+							  ?>
+						  </dd>
 
 						  <dt>E-mail</dt>
-						  <dd><a href="position-detail.html#">info@example.com</a></dd>
+						  <dd>
+							  <a href="mailto:<?php echo $product_info['customers_email_address'];?>">
+								  <?php echo $product_info['customers_email_address'];?>
+							  </a>
+						  </dd>
 
 						  <dt>Phone</dt>
-						  <dd>1-234-456-789</dd>
+						  <dd><?php echo $product_info['customers_telephone'];?></dd>
 
 						  <dt>Address</dt>
 						  <dd>
-							  Everton Street 231,<br>
-							  San Francisco, California
+							  <?php echo $product_info['customers_address'];?>
 						  </dd>
 					  </dl>
 				  </div><!-- /.company-card-data -->
@@ -191,20 +187,20 @@
 			  <div class="widget">
 				  <h2>Apply For Position</h2>
 
-				  <form method="get" action="position-detail_2.html">
+				  <form>
 					  <div class="form-group">
-						  <input type="text" class="form-control" placeholder="Subject">
+						  <input type="text" id="name" class="form-control" placeholder="Name" required>
 					  </div><!-- /.form-group -->
 
 					  <div class="form-group">
-						  <input type="text" class="form-control" placeholder="Your E-mail">
+						  <input type="email" class="form-control" id="email" placeholder="Your E-mail" required>
 					  </div><!-- /.form-group -->
 
 					  <div class="form-group">
-						  <textarea class="form-control" rows="5" placeholder="Your Message"></textarea>
+						  <textarea class="form-control" rows="5" id="text" placeholder="Your Message" required></textarea>
 					  </div><!-- /.form-group -->
 
-					  <button class="btn btn-secondary pull-right" type="submit">Apply Now</button>
+					  <button class="btn btn-secondary pull-right" type="submit" id="sendEmail">Apply Now</button>
 				  </form>
 			  </div><!-- /.widget -->
 		  </div><!-- /.col-* -->
@@ -218,38 +214,30 @@
   require(DIR_WS_INCLUDES . 'template_bottom.php');
   require(DIR_WS_INCLUDES . 'application_bottom.php');
 ?>
-<script
-	type="text/javascript"
-	src="ext/ng/lib/angular/1.3.15/angular.min.js"
-></script>
 <script>
-	var app = angular.module('main', []);
-	app.controller(
-		'send_mail_ctrl', [
-			'$scope'
-			, '$http'
-			, function ($scope, $http){
-				$scope.language_id = '';
-				$scope.sendMail = function(){
-					var params = {
-						name: $scope.name,
-						email: $scope.email,
-						enquiry: $scope.enquiry
-					};
-					console.log(params);
-					$http({
-						url: 'api/SendMail',
-						method: 'POST',
-						headers: {
-							'Content-Type': undefined
-						},
-						data: JSON.stringify(params)
-					}).success(function(data){
-						console.log(data);
-					});
-				};
 
+	$(function() {
+		$('form').submit(function (e) {
+			var form = {
+				name: $('#name').val(),
+				email: $('#email').val(),
+				enquiry: $('#text').val()
+			};
+			e.preventDefault();
+			console.log(form);
+			$.ajax({
+				type: 'POST',
+				url: 'api/SendMail',
+				data: form,
+				success: function (response) {
+					console.log(response);
+					if (response == 0) {
+						// ============================ Not here, this would be too late
+						span.text('email does not exist');
+					}
+				}
+			});
+		});
+	});
 
-			}
-		]);
 </script>
