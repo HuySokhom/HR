0b3fa0b0ea4667e32b6a8afde1a48b2c9f4a6890
sokhom <sokhomhuy.corarl@gmail.com>
2017-01-03 00:00:50 +0700

    update master

--- ext/ng/app/lesson/controller/lesson_ctrl.js
+++ ext/ng/app/lesson/controller/lesson_ctrl.js
@@ -4,16 +4,16 @@ app.controller(
 	, 'Restful'
 	, function ($scope, Restful){
 		var vm = this;
-		var params = {pagination: 'yes'};
+		vm.params = {pagination: 'yes'};
 		var url = 'api/Leason/';
 		vm.init = function(params){
 			Restful.get(url, params).success(function(data){
 				vm.data = data;
-				console.log(data);
-				$scope.totalItems = data.count;
+				//console.log(data);
+				vm.totalItems = data.count;
 			});
 		};
-		vm.init(params);
+		vm.init(vm.params);
 
 		vm.renderLink = function(id, link){
 			var replace = link.replace(/[^a-zA-Z ]/g, "_");
@@ -28,8 +28,8 @@ app.controller(
 		//get another portions of data on page changed
 		vm.pageChanged = function() {
 			vm.pageSize = 10 * ( vm.currentPage - 1 );
-			vm.start = vm.pageSize;
-			vm.init(params);
+			vm.params.start = vm.pageSize;
+			vm.init(vm.params);
 		};
 	}
 ]);
